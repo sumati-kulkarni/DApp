8321917f0d9dbd3e3a09edc7ebfa6851dafa945a
Sumati <sumati@localhost.localdomain>
2020-04-16 15:45:48 -0400

    Update Exchange Token

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -18,30 +18,55 @@ pragma solidity ^0.5.0;
 
 import "./Token.sol";
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 /**
  * The contractName contract does this and that...
  */
 contract Exchange {
+	using SafeMath for uint;
 
 	// Variables
 	address public feeAccount; // the account that receives exchange
 	uint256 public feePercent; // the fee percentage
+
+	address constant ETHER = address(0); // store Ether in tokens mapping with blank address
+
+	// first key - all the tokens that has been deposited address, token addresses
+	// second key - address of the user who has deposited the tokens themselves
+	mapping(address => mapping(address => uint256)) public tokens;
+
+	// Events
+	event Deposit(address token, address user, uint256 amount, uint256 balance);
 
 	constructor(address _feeAccount, uint256 _feePercent) public {
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
 	}
 
+	function depositEther() payable public {
+		// solidity allows you to send ether with any function call
+		// mag.value also accepts ether. therefore we should have payable
+		tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+		emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+	}
+
 	function depositToken(address _token, uint _amount) public {
+		// TODO: Dont allow Ether Deposits
+		require (_token != ETHER);
+		
 		// which token? This can be any ERC Token - as parameter
 		// how much token to deposit - as parameter
 		// this represents smart contract in solidity
-		Token(_token).transferFrom(msg.sender, address(this), _amount);
 		// send tokens to this contract
+		require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 
-		// manage balance of exchange
-
+		// manage deposit - update balance
+		// default will be zero if no tokens
+		tokens[_token][msg.sender]  = tokens[_token][msg.sender].add(_amount);
+		
 		// emit event
+		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
 	}
 }
 
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract('Exchange', ([deployer, feeAccount], user1) => { 
+contract('Exchange', ([deployer, feeAccount, user1]) => { 
 	let token
 	// to make exchange available to every test case 
 	let exchange
@@ -15,9 +15,12 @@ contract('Exchange', ([deployer, feeAccount], user1) => {
 	
 	beforeEach(async () => {
 		token = await Token.new()
-		// Fetch account from blockchain
+		// Transfer some tokens to user1
+		token.transfer(user1, tokens(100), { from: deployer })
+		// Fetch account from blockchain to deploy exchange
 		exchange = await Exchange.new(feeAccount, feePercent)
 	})
+
 	describe('deployment', () => {
 		it('tracks the fee account', async () => {
 			// Read token name here
@@ -42,7 +45,7 @@ contract('Exchange', ([deployer, feeAccount], user1) => {
 		beforeEach(async () => {
 			amount = tokens(10)
 			await token.approve(exchange.address, tokens(10), { from: user1})
-			const result = await exchange.depositToken(token.address, tokens(10), { from: user1 })
+			result = await exchange.depositToken(token.address, tokens(10), { from: user1 })
 		})
 
 		describe('success', () => {
@@ -51,10 +54,31 @@ contract('Exchange', ([deployer, feeAccount], user1) => {
 				let balance
 				balance = await token.balanceOf(exchange.address)
 				balance.toString().should.eq(amount.toString())
+				// check tokens on exchange
+				balance = await exchange.tokens(token.address, user1)
+				balance.toString().should.eq(amount.toString())
+			})
+
+			it('emits a Deposit event', async () => {
+				// console.log(result.logs)
+				const log = result.logs[0]
+				log.event.should.eq('Deposit')
+				const event = log.args
+				// good to have these coz if something is wrong truffle will tell you
+				event.token.should.eq(token.address, 'token address is correct')
+				event.user.should.eq(user1, 'user address is correct')
+				event.amount.toString().should.eq(tokens(10).toString(), 'amount is correct')
+				event.balance.toString().should.equal(tokens(10).toString(), 'balance is correct')
 			})
 		})
 		describe('failure', () => {
+			it('rejects ether deposits', async() => {
+				await exchange.depositToken(ETHER_ADDRESS, tokens(10), { from: user1}).should.be.rejectedWith(EVM_REVERT)
+			})
 
+			it('fails when no tokens are approved', async () => {
+				await exchange.depositToken(token.address, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+			})
 		})
 	})
 })
